b8024ba7f8bb5dd0ec7465240175f79b03ca1721
Neeraj Kumar Saroya <neeraj.saroya@zuehlke.com>
2022-05-11 13:03:12 +0200

    Adding time controller

--- src/main/java/com/example/jenkinsdemo/HelloController.java
+++ src/main/java/com/example/jenkinsdemo/HelloController.java
@@ -5,11 +5,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/")
     public ResponseEntity<String> hello() {
         return new ResponseEntity<>("Hello World", HttpStatus.OK);
+    }
+    @GetMapping("/time")
+    public ResponseEntity<String> getTime() {
+        return new ResponseEntity<>(new Date().toString(), HttpStatus.OK);
     }
 }
